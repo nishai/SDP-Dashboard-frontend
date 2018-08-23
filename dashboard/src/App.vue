8ec0b2e289879b656eaea271e122c35e66cac803
Nishai Kooverjee <nishai.kooverjee@gmail.com>
2018-08-23 19:08:28 +0200

    Added a sidebar

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    <h1>"Blank Site"</h1>
-    <h2>by Team Potato🥔</h2>
+    <div id="sidebar" class="sidenav">
+      <a href="#">Template 1</a>
+      <a href="#">Template 2</a>
+      <a href="#">Template 3</a>
+      <a href="#">Template 4</a>
+    </div>
+    <div id="stuff">
+      <h1>"Blank Site"</h1>
+      <h2>by Team Potato🥔</h2>
+    </div>
   </div>
 </template>
 
@@ -21,8 +29,31 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#sidebar {
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 200px;
+  position: fixed;
+  position: fixed;
+  background-color: whitesmoke;
+}
+
+#sidebar a{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: black;
+  font-size: 20px;
+  padding: 10px 30px;
+  text-decoration: none;
+  display: block;
+}
+
+#stuff {
+  text-align :center;
+  margin-left: 200px;
 }
 </style>
