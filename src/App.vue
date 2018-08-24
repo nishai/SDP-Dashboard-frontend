261949cd3098695157af979031350ee20a2893bb
Nishai Kooverjee <nishai.kooverjee@gmail.com>
2018-08-24 22:14:53 +0200

    Used Vue component for Sidebar instead of HTML

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-    <div id="sidebar" class="sidenav">
-      <a href="#">Template 1</a>
-      <a href="#">Template 2</a>
-      <a href="#">Template 3</a>
-      <a href="#">Template 4</a>
-    </div>
+    <Sidebar/>
     <div id="stuff">
       <h1>"Blank Site"</h1>
       <h2>by Team Potato🥔</h2>
@@ -14,12 +9,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Sidebar from './components/Sidebar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
@@ -31,25 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-#sidebar {
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 200px;
-  position: fixed;
-  position: fixed;
-  background-color: whitesmoke;
-}
-
-#sidebar a{
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  color: black;
-  font-size: 20px;
-  padding: 10px 30px;
-  text-decoration: none;
-  display: block;
 }
 
 #stuff {
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="sidebar" class="sidenav">
+      <a href="#">Template 1</a>
+      <a href="#">Template 2</a>
+      <a href="#">Template 3</a>
+      <a href="#">Template 4</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+}
+</script>
+
+<style>
+#sidebar {
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 200px;
+  position: fixed;
+  position: fixed;
+  background-color: whitesmoke;
+}
+
+#sidebar a{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: black;
+  font-size: 20px;
+  padding: 10px 30px;
+  text-decoration: none;
+  display: block;
+}
+
+</style>
