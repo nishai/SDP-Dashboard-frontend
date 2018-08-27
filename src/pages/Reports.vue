b92216dd4996fb08b8c323ae1064e06e4e70557c
Nathan Michlo <1386161@students.wits.ac.za>
2018-08-27 16:28:03 +0200

    Api Request Example

--- src/pages/Reports.vue
+++ src/pages/Reports.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="reports">
-    <Heading heading_text="Example Chart"></Heading>
+    <Heading heading_text="Reports - API Request Example"></Heading>
     <p id="content"> This is just an example page </p>
-    <Chart></Chart>
+    <p id="content"> {{ info }} </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Heading from '../components/Heading.vue';
 import Chart from '../components/Chart.vue';
 
@@ -16,7 +17,23 @@ export default {
     Heading,
     Chart,
   },
+  data () {
+    return {
+      info: "Loading Content from: http://api.icndb.com/jokes/random. Please wait!"
+    }
+  },
+  mounted () {
+    axios
+      .get('http://api.icndb.com/jokes/random')
+      .then(response => {
+        this.info = response.data.value.joke;
+      })
+      .catch(response => {
+        this.info = "(Unable to load a joke) [Error]: " + response
+      });
+  }
 };
+
 </script>
 
 <style>
