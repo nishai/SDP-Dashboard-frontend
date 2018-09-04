caf50dd5706241436f3db29f5a35a0be293f8161
Nishai Kooverjee <nishai.kooverjee@gmail.com>
2018-09-04 12:23:05 +0200

    Added superimpose button

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -3,6 +3,7 @@
     <Heading heading_text="Example Dashboard"></Heading>
     <p id="content"> This is just an example page </p>
     <Chart></Chart>
+    <Button id="super" @click="superimpose">Superimpose</Button>
   </div>
 </template>
 
@@ -15,6 +16,25 @@ export default {
   components: {
     Heading,
     Chart,
+  },
+  methods: {
+    superimpose(event) {
+      const new_dataset = {
+        data: [55, 8, 12, 18, 7],
+        backgroundColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 2,
+      };
+
+      Chart.data().chart_data.data.datasets.push(new_dataset);
+      console.log(Chart.data().chart_data.data.datasets);
+      document.getElementById('super').disabled = true;
+    },
   },
 };
 </script>
