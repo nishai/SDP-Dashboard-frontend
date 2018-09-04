089de0341143ad4d2ea4784ddf8ef7dee1aceaba
Nishai Kooverjee <nishai.kooverjee@gmail.com>
2018-09-04 20:20:43 +0200

    Used vue-chartjs for chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,59 +1,33 @@
-/* eslint-disable */
-<template>
-    <div id="chart_div">
-        <canvas id="chart"></canvas>
-    </div>
-</template>
-
 <script>
-import Chart from 'chart.js';
+import { Doughnut } from 'vue-chartjs';
 
 export default {
-  name: 'Chart',
-  data() {
-    return {
-      chart_data: {
-        type: 'doughnut',
-        data: {
-          title: 'Race',
-          labels: ['Black', 'White', 'Indian', 'Coloured', 'Other'],
-          datasets: [{
-            data: [60, 13, 12, 10, 5],
-            backgroundColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-            ],
-            borderWidth: 2,
-          }],
-        },
-        options: {
-          title: {
-            display: true,
-            text: 'Course Demographics: Race',
-          },
+  extends: Doughnut,
+  mounted() {
+    this.renderChart(
+      {
+        labels: ['Black', 'White', 'Indian', 'Coloured', 'Other'],
+        datasets: [{
+          data: [60, 13, 12, 10, 5],
+          backgroundColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+          ],
+          borderWidth: 2,
+        }],
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: 'Course Demographics: Race',
         },
       },
-    };
-  },
-  mounted() {
-    const ctx = 'chart';
-    const chart = new Chart(ctx, this.chart_data);
+    );
   },
 };
 </script>
-
-<style>
-/* #chart_div { */
-    /* margin-left: 200px;
-    width: calc(100% - 200px);
-    height: 100%;
-    border: 1px solid black; */
-/* } */
-/* #chart { */
-    /* width: 400px; */
-    /* border: 1px solid red; */
-/* } */
-</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -19,21 +19,6 @@ export default {
   },
   methods: {
     superimpose(event) {
-      const new_dataset = {
-        data: [55, 8, 12, 18, 7],
-        backgroundColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 2,
-      };
-
-      Chart.data().chart_data.data.datasets.push(new_dataset);
-      console.log(Chart.data().chart_data.data.datasets);
-      document.getElementById('super').disabled = true;
     },
   },
 };
