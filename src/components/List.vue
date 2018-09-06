b6e85a016734f033be6491f973b574d948632d62
Matthew Cockcroft <cockcroft.matthew@gmail.com>
2018-09-06 11:09:12 +0200

    added list functionality working

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="List">
     <select v-model="selectedColumn">
-      <option v-for="item in items" :key="item">{{item}}</option>
+      <!--option v-for="student in students" :key="student.id">{{Object.keys(student)}}</option-->
+      <option v-for="item in itemID" :key="item">{{item}}</option>
     </select>
     <h2>{{selectedColumn}}</h2>
-    <!--TODO-->
-    <!--Rather than studentNo I want to loop through the selectedColumn-->
-    <!--Issue is that that is a string and I'm not sure how to replace it with the relevant array-->
-    <li v-for="item in studentNo" :key="item">
-      {{item}}
+    <li v-if="selectedColumn==='studentNo'" v-for="student in students" :key="student.id">
+      {{student.studentNo}}
+    </li>
+    <li v-if="selectedColumn==='school'" v-for="student in students" :key="student.id">
+      {{student.school}}
+    </li>
+    <li v-if="selectedColumn==='averageMark'" v-for="student in students" :key="student.id">
+      {{student.averageMark}}
     </li>
   </div>
 </template>
@@ -18,11 +22,23 @@ export default {
   name: 'List',
   data() {
     return {
-      studentNo: ['0021D31BE03E4AB097DCF9C0C89B13BA', '002420AA1391E39E6D8FE424D96BCB51', '0033DA3F89CE5F5306C06E04A808E9C3', '004D87C05B047CBA6C54582FAAB34ED3'],
-      school: ['Forte Secondary School', 'Mcauley House School', 'Khumbula Secondary School', 'Altmont Technical High School'],
-      averageMark: ['60.75', '72.33', '39.3', '53.25'],
-      items: ['StudentNo', 'School', 'AverageMark'],
+      students: [
+        {
+          id: 1, studentNo: '0021D31BE03E4AB097DCF9C0C89B13BA', school: 'Forte Secondary School', averageMark: '60.75',
+        },
+        {
+          id: 2, studentNo: '002420AA1391E39E6D8FE424D96BCB51', school: 'Mcauley House School', averageMark: '72.33',
+        },
+        {
+          id: 3, studentNo: '0033DA3F89CE5F5306C06E04A808E9C3', school: 'Khumbula Secondary School', averageMark: '39.3',
+        },
+        {
+          id: 4, studentNo: '004D87C05B047CBA6C54582FAAB34ED3', school: 'Altmont Technical High School', averageMark: '53.25',
+        },
+      ],
       selectedColumn: '',
+      value: [],
+      itemID: ['studentNo', 'school', 'averageMark'],
     };
   },
 };
