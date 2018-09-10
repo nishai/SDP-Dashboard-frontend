c7f340f6136cb03576840e67e3c73e2227ce6f6f
Nathan Michlo <1386161@students.wits.ac.za>
2018-09-10 17:35:36 +0200

    Added vuedraggable package

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     host: '0.0.0.0',
     port: '3080',
     public: 'dashboard-dev.ms.wits.ac.za:3080',
+    // https://cli.vuejs.org/config/#devserver-proxy
     proxy: {
       '/api': {
         // More Info: https://github.com/chimurai/http-proxy-middleware
