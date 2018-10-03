c7b7320cbdbd828eb7df1a34e2d0b0e1e47b0f0f
ohanoch <orhanoch@protonmail.com>
2018-10-03 13:47:58 +0200

    made form variable dynamic + fixed eslint errors

--- src/components/modal/FilterForm.vue
+++ src/components/modal/FilterForm.vue
@@ -3,23 +3,51 @@
   <b-form @submit="onSubmit" @reset="onClose" v-if="show">
     <b-row>
       <b-col v-for="i in numForms" :key="i">
-        <b-form-group v-if="ftype === true" id="typeGroup" label="Chart Type:" label-for="Type" horizontal>
+        <b-form-group
+          v-if="ftype === true"
+          id="typeGroup"
+          label="Chart Type:"
+          label-for="Type"
+          horizontal>
           <b-form-select id="Type" :options="types" required v-model="form.type[i-1]">
           </b-form-select>
         </b-form-group>
-        <b-form-group v-if="fyear === true" id="YearGroup" label="Year:" label-for="Year" horizontal>
+        <b-form-group
+          v-if="fyear === true"
+          id="YearGroup"
+          label="Year:"
+          label-for="Year"
+          horizontal>
           <b-form-select id="Year" :options="years" required v-model="form.year[i-1]">
           </b-form-select>
         </b-form-group>
-        <b-form-group v-if="ffaculty === true" id="FacultyGroup" label="Faculty:" label-for="Faculty" description="You can choose multiple faculties using Ctrl key" horizontal>
+        <b-form-group
+          v-if="ffaculty === true"
+          id="FacultyGroup"
+          label="Faculty:"
+          label-for="Faculty"
+          description="You can choose multiple faculties using Ctrl key"
+          horizontal>
           <b-form-select multiple id="Faculty" :options="faculties" required v-model="form.faculty[i-1]">
           </b-form-select>
         </b-form-group>
-        <b-form-group v-if="fschool === true && form.faculty[i-1] !== null" id="SchoolGroup" label="School:" label-for="School" description="You can choose multiple schools using Ctrl key" horizontal>
+        <b-form-group
+          v-if="fschool === true && form.faculty[i-1] !== null"
+          id="SchoolGroup"
+          label="School:"
+          label-for="School"
+          description="You can choose multiple schools using Ctrl key"
+          horizontal>
           <b-form-select multiple id="School" :options="schools" required v-model="form.school[i-1]">
           </b-form-select>
         </b-form-group>
-        <b-form-group v-if="fcourse === true && form.school[i-1] !== null" id="CourseGroup" label="Course:" label-for="Course" description="You can choose multiple courses using Ctrl key" horizontal>
+        <b-form-group
+          v-if="fcourse === true && form.school[i-1] !== null"
+          id="CourseGroup"
+          label="Course:"
+          label-for="Course"
+          description="You can choose multiple courses using Ctrl key"
+          horizontal>
           <b-form-select multiple id="Course" :options="courses" required v-model="form.course[i-1]">
           </b-form-select>
         </b-form-group>
@@ -41,11 +69,11 @@ export default {
   data() {
     return {
       form: {
-        type: [null, null],
-        year: [null, null],
-        course: [null, null],
-        faculty: [null, null],
-        school: [null, null],
+        type: [],
+        year: [],
+        course: [],
+        faculty: [],
+        school: [],
       },
       types: [
         { text: 'Select One', value: null },
@@ -69,6 +97,14 @@ export default {
       ],
       show: true,
     };
+  },
+  mounted() {
+    const keys = Object.keys(this.form);
+    for (let i = 0; i < keys.length; i += 1) {
+      for (let j = 0; j < this.$props.numForms; j += 1) {
+        this.form.keys[i].push(null);
+      }
+    }
   },
   methods: {
     onSubmit(evt) {
