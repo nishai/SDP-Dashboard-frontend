80b53352331492b1f20b3b52c2879a275bbd4bb9
Matthew Cockcroft <cockcroft.matthew@gmail.com>
2018-10-08 14:13:48 +0200

    Added sortability

--- src/components/charts/chartjs/Table.vue
+++ src/components/charts/chartjs/Table.vue
@@ -1,31 +1,63 @@
 <template>
-    <b-table id="table" striped hover :items="rows"></b-table>
+    <b-table id="table" striped hover :items="rows" :fields="fields"></b-table>
 </template>
 
 <script>
 
-const rows = [
-  {
-    id: 1, studentNo: '0021D31BE03E4AB097DCF9C0C89B13BA', school: 'Forte Secondary School', averageMark: '60.75',
-  },
-  {
-    id: 2, studentNo: '002420AA1391E39E6D8FE424D96BCB51', school: 'Mcauley House School', averageMark: '72.33',
-  },
-  {
-    id: 3, studentNo: '0033DA3F89CE5F5306C06E04A808E9C3', school: 'Khumbula Secondary School', averageMark: '39.3',
-  },
-  {
-    id: 4, studentNo: '004D87C05B047CBA6C54582FAAB34ED3', school: 'Altmont Technical High School', averageMark: '53.25',
-  },
-  {
-    id: 5, studentNo: '004F3079DFD6018586D2D2BC04713484', school: 'Mbilwi Snr Sec School', averageMark: '65',
-  },
-];
 export default {
   name: 'Table',
   data() {
     return {
-      rows,
+      fields: [
+        {
+          key: 'id',
+          sortable: true,
+        },
+        {
+          key: 'studentNo',
+          sortable: true,
+        },
+        {
+          key: 'school',
+          sortable: true,
+        },
+        {
+          key: 'averageMark',
+          sortable: true,
+        },
+      ],
+      rows: [
+        {
+          id: 1,
+          studentNo: '0021D31BE03E4AB097DCF9C0C89B13BA',
+          school: 'Forte Secondary School',
+          averageMark: '60.75',
+        },
+        {
+          id: 2,
+          studentNo: '002420AA1391E39E6D8FE424D96BCB51',
+          school: 'Mcauley House School',
+          averageMark: '72.33',
+        },
+        {
+          id: 3,
+          studentNo: '0033DA3F89CE5F5306C06E04A808E9C3',
+          school: 'Khumbula Secondary School',
+          averageMark: '39.3',
+        },
+        {
+          id: 4,
+          studentNo: '004D87C05B047CBA6C54582FAAB34ED3',
+          school: 'Altmont Technical High School',
+          averageMark: '53.25',
+        },
+        {
+          id: 5,
+          studentNo: '004F3079DFD6018586D2D2BC04713484',
+          school: 'Mbilwi Snr Sec School',
+          averageMark: '65',
+        },
+      ],
     };
   },
 };
