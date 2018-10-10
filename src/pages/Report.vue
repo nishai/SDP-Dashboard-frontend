660976c9418f0c4034abe992b7625da7281066c1
Nishai Kooverjee <nishai.kooverjee@gmail.com>
2018-10-10 17:45:16 +0200

    Used html2canvas and made the pdf report work

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -2,9 +2,9 @@
   <div ref="reportPage">
     <Heading :heading_text="report.name"></Heading>
     <h3> {{ report.desc }} </h3>
-    <b-button class="btn btn-info" @click="doPrint">Print report</b-button>
+    <b-button class="btn btn-info" @click="doPDF">Save report</b-button>
     <div class="d-flex flex-wrap justify-content-start">
-      <ReportChart style="width: 400px;" class="m-2" v-for="(chart, id) in report.charts" :key="id" :chartId="id" :chart="chart"></ReportChart>
+      <ReportChart ref="reportPage" style="width: 400px;" class="m-2" v-for="(chart, id) in report.charts" :key="id" :chartId="id" :chart="chart"></ReportChart>
       <div style="width: 400px;" class="d-flex justify-content-center">
         <b-button class="my-4 mx-2" style="height: 48px;" variant="outline-success" size="lg" @click="create"> New Chart </b-button>
       </div>
@@ -16,6 +16,7 @@
 import Heading from '../components/misc/Heading.vue';
 import ReportChart from '../components/reports/ReportChart.vue';
 import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 export default {
   name: 'Report',
@@ -35,18 +36,19 @@ export default {
     create() {
       this.$store.dispatch('createReportChart', { reportId: this.reportId });
     },
-    doPrint() {
-      const doc = new jsPDF();
-      const specialElementHandlers = {
-        '#editor': function (element, renderer) {
-          return true;
-        },
-      };
+    doPDF() {
+      var ratio = this.$refs.reportPage.clientHeight / this.$refs.reportPage.clientWidth;
 
-      doc.fromHTML(this.$refs.reportPage[0], 15, 15, {
-        width: 170, elementHandlers: specialElementHandlers,
-      });
-      doc.save('report.pdf');
+      html2canvas(this.$refs.reportPage)
+        .then(canvas => {  
+          var imgData = canvas.toDataURL('image/png');              
+          var doc = new jsPDF();
+          var width = doc.internal.pageSize.getWidth(); 
+          var height = doc.internal.pageSize.getHeight(); 
+          height = ratio * width;
+          doc.addImage(imgData, 'PNG', 0, 0, width-20, height-10);
+          doc.save('report.pdf');
+        });
     },
   },
 };
