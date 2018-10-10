19fffe981275d6267d651404bc9ad0e4e9faccd1
beevabeeva <avibank1@gmail.com>
2018-10-10 16:00:05 +0200

    Resizeable CHARTSSSSgit status

--- src/pages/GridExample.vue
+++ src/pages/GridExample.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div class="Grid">
+ <div class="Grid">
 
       <grid-layout
             :layout="layout"
@@ -21,19 +21,19 @@
                    :w="item.w"
                    :h="item.h"
                    :i="item.i">
-                    {{item.i}}
+                    <!-- {{item.i}} -->
  
 
   <div class="dashboardChart">
-    <div class="d-flex flex-wrap justify-content-between">
-      <div class="d-flex flex-wrap justify-content-start">
-        <DashboardChart style="width: 400px;" class="m-2" v-for="(chart, id) in dashboardCharts" v-bind:key="id" v-bind:dashboardChartId="id" v-bind:chart="chart"  ></DashboardChart>
+    <!-- <div class="d-flex flex-wrap justify-content-between"> -->
+      <!-- <div class="d-flex flex-wrap justify-content-start"> -->
+        <DashboardChart style="width: 100%;height: 100%;position:relative;" class="m-2" v-for="(chart, id) in dashboardCharts" v-bind:key="id" v-bind:dashboardChartId="id" v-bind:chart="chart"  ></DashboardChart>
         <div style="width: 400px;" class="d-flex justify-content-center">
           <!-- <router-link :to="{path: '/'}"> -->
           <!--   <b-button class="my-4 mx-2" style="height: 48px;" variant="outline-success" size="lg" @click="create"> New Chart </b-button> -->
           <!-- </router-link> -->
         </div>
-      </div>
+      <!-- </div> -->
       <div>
       <!-- <b-button class="my-4 mx-2" style="height: 48px;" variant="outline-success" size="lg" v-on:click="add_table = true"> New Table </b-button>
         <Table v-if="add_table"></Table> -->
@@ -43,14 +43,13 @@
         <List v-if="add_list"></List> -->
       </div>
     </div>
-  </div>
+  <!-- </div> -->
 
 
 
 
        </grid-item>
     </grid-layout>
-  
 </div>
 
 </template>
@@ -66,11 +65,7 @@ import { GridItem } from 'vue-grid-layout';
 
 
 var testLayout = [
-        {"x":0,"y":0,"w":2,"h":5,"i":"0"},
-        {"x":2,"y":0,"w":2,"h":5,"i":"1"},
-        {"x":4,"y":0,"w":2,"h":5,"i":"2"},
-        {"x":6,"y":0,"w":2,"h":5,"i":"3"},
-        {"x":8,"y":0,"w":2,"h":5,"i":"4"}
+        {"x":0,"y":0,"w":5,"h":27,"i":"0"},
     ];
 
 
