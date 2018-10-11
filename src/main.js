9b21215f186076be2dce0ab182266b52b9c29ca2
Nathan Michlo <1386161@students.wits.ac.za>
2018-10-11 22:24:31 +0200

    Fixed vars

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+/* Check Mode */ /* eslint-disable */
+console.log(`NODE_ENV: "${process.env.NODE_ENV}"`);
+console.log(`BASE_URL: "${process.env.BASE_URL}"`);
+console.log(`VUE_APP_URL_BASE: "${process.env.VUE_APP_URL_BASE}"`);
+console.log(`VUE_APP_API_BASE: "${process.env.VUE_APP_API_BASE}"`);
 
 /* add miscellaneous plugins to Vue */
 
