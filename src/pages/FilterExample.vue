55028d8c1f84cc09cf2c659306d17732605d2046
beevabeeva <avibank1@gmail.com>
2018-10-24 15:18:49 +0200

    made progress - after experimenting with 3 other vue filtertag things i think i found one that works.

---
+++ src/pages/FilterExample.vue
@@ -0,0 +1,155 @@
+<!-- <template> -->
+<!--   <div>
+    <multiselect
+      v-model="selected"
+      :options="options">
+    </multiselect>
+  </div> -->
+
+ <!--  <div>
+  <label class="typo__label">Groups</label>
+  <multiselect v-model="Fuckoptions" :options="Fuckoptions" :multiple="true" group-values="libs" group-label="language" :group-select="true" placeholder="Type to search" track-by="name" label="name"><span slot="noResult">Oops! No elements found. Consider changing the search query.</span></multiselect>
+  <pre class="language-json"><code>{{ value }}</code></pre>
+</div>
+
+
+
+
+</template> -->
+<!-- 
+<script>
+ import Multiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      value: [
+        { name: 'Javascript', code: 'js' }
+      ],
+      Fuckoptions: [
+        { name: 'Vue.js', code: 'vu' },
+        { name: 'Javascript', code: 'js' },
+        { name: 'Open Source', code: 'os' }
+      ],
+    }
+  },
+  methods: {
+    addTag (newTag) {
+      const tag = {
+        name: newTag,
+        code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
+      }
+      this.options.push(tag)
+      this.value.push(tag)
+    }
+  }
+}
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style> -->
+
+
+
+
+<!-- <template>
+  <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      @tags-changed="newTags => tags = newTags"
+    />
+  </div>
+</template>
+
+<script>
+import VueTagsInput from '@johmun/vue-tags-input';
+
+export default {
+  components: {
+    VueTagsInput,
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [],
+    };
+  },
+};
+</script>
+         -->
+
+
+
+
+
+<template>
+  <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      :autocomplete-items="filteredItems"
+      @tags-changed="newTags => tags = newTags">
+    </vue-tags-input>
+
+    <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      :autocomplete-items="filteredItems2"
+      @tags-changed="newTags => tags = newTags">
+    </vue-tags-input>
+
+
+  </div>
+</template>
+
+<script>
+import VueTagsInput from '@johmun/vue-tags-input';
+
+export default {
+  components: {
+    VueTagsInput,
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [],
+      autocompleteItems: [{
+        text: 'Spain',
+      }, {
+        text: 'France',
+      }, {
+        text: 'USA',
+      }, {
+        text: 'Germany',
+      }, {
+        text: 'China',
+      }],
+
+      autocompleteItems2: [{
+        text: 'Spain',
+      }, {
+        text: 'France',
+      }, {
+        text: 'USA',
+      }, {
+        text: 'Germany',
+      }, {
+        text: 'China',
+      }],
+    };
+  },
+  computed: {
+    filteredItems() {
+      return this.autocompleteItems.filter(i => new RegExp(this.tag, 'i').test(i.text));
+    },
+    filteredItems2() {
+      return this.autocompleteItems2.filter(i => new RegExp(this.tag, 'i').test(i.text));
+    },
+  },
+};
+</script>
+        
