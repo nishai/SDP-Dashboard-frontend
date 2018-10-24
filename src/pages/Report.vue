e3aa16721ed4780a4f1111e11373784f26d7c0cd
Nishai Kooverjee <nishai.kooverjee@gmail.com>
2018-10-24 19:09:37 +0200

    pre-merge commit

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -4,7 +4,7 @@
     <h3> {{ report.desc }} </h3>
     <b-button class="btn btn-info" @click="doPDF">Save report</b-button>
     <div class="d-flex flex-wrap justify-content-start">
-      <ReportChart ref="reportPage" style="width: 400px;" class="m-2" v-for="(chart, id) in report.charts" :key="id" :chartId="id" :chart="chart"></ReportChart>
+      <ReportChart ref="single_report" style="width: 400px;" class="m-2" v-for="(chart, id) in report.charts" :key="id" :chartId="id" :chart="chart"></ReportChart>
       <div style="width: 400px;" class="d-flex justify-content-center">
         <b-button class="my-4 mx-2" style="height: 48px;" variant="outline-success" size="lg" @click="create"> New Chart </b-button>
       </div>
@@ -37,18 +37,23 @@ export default {
       this.$store.dispatch('createReportChart', { reportId: this.reportId });
     },
     doPDF() {
-      var ratio = this.$refs.reportPage.clientHeight / this.$refs.reportPage.clientWidth;
+      // var ratio = this.$refs.reportPage.clientHeight / this.$refs.reportPage.clientWidth;
+      // var doc = new jsPDF();
+      // var width = doc.internal.pageSize.getWidth(); 
+      // var height = doc.internal.pageSize.getHeight(); 
+      // height = ratio * width;
 
-      html2canvas(this.$refs.reportPage)
-        .then(canvas => {  
-          var imgData = canvas.toDataURL('image/png');              
-          var doc = new jsPDF();
-          var width = doc.internal.pageSize.getWidth(); 
-          var height = doc.internal.pageSize.getHeight(); 
-          height = ratio * width;
-          doc.addImage(imgData, 'PNG', 0, 0, width, height, '', 'FAST');
-          doc.save('report.pdf');
-        });
+      // for (var chart in this.$store.state.reports.reports[this.reportId]){
+      //   html2canvas(chart)
+      //   .then(canvas => {  
+      //     var imgData = canvas.toDataURL('image/png');              
+      //     doc.addImage(imgData, 'PNG', 0, 0, 0, 0, '', 'FAST');
+      //   });
+      // }
+
+      // doc.save('report.pdf');
+
+      console.log(this.report.charts);
     },
   },
 };
